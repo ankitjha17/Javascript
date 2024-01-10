67aa3676f21418d85847566441cbbbceed241112
ankitjha17 <97396656+ankitjha17@users.noreply.github.com>
2024-01-10 08:12:52 +0000

    added variable file

---
+++ 01_basics/variables.js
@@ -0,0 +1,14 @@
+const accountID = 1234465;
+//  const variable cannot be change once it's have been declared
+
+let accountEmail = 'ajha@google.com';
+// if we declare the variable let then it can only be accessilbe with their scope
+
+var accountPassword = '12455';
+// if we declare the varialbe with var then it can we changed & it will create problems in code. 
+
+/* Const & let - ES6(introduce in)
+var - ES5 */
+
+// console.log(accountEmail)
+// console.table([accountID,accountEmail,accountPassword])
