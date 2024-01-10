b3cb83c5cb25d400e4ddb07d730995b5f26a6331
ankitjha17 <97396656+ankitjha17@users.noreply.github.com>
2024-01-10 13:38:13 +0000

    comparsion

---
+++ 01_basics/comparison.js
@@ -0,0 +1,19 @@
+//console.log(2 > 1);
+//console.log(2 >= 1);
+//console.log(2 < 1);
+//consol.log(2 == 1);
+//console.log(2 !==1);
+
+/* one thing to keep in mind that when we compare - that both data should be of a single type
+
+for example -
+we  are comparing number then compare it with number only 
+*/
+
+//intervirew can ask
+//null is empty
+console.log(null > 0); //false
+console.log(null == 0); //false
+console.log(null >= 0); //true
+/* for this last console, comparisons convert the null to a number, treating it as 0.
+That's why (null >= 0) is true and (null > 0) is false */
