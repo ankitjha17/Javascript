1e9aee5bf1c24c4433f917f20fefc14e7b5ebaab
ankitjha17 <97396656+ankitjha17@users.noreply.github.com>
2024-01-10 09:40:49 +0000

    dataTypes

---
+++ 01_basics/dataType.js
@@ -0,0 +1,20 @@
+"use Strict"; //mainly use to treat all js code as newer version
+
+//premative
+
+//numer - 
+//Bigint
+//Boolean
+//undefined 
+//null - ye can assign null to a variable (Data Type of null -  Object)
+//string
+//symbol - unique 
+
+
+//nom-premative
+
+//object
+//array
+
+// to check the data type we can write
+// console.log(typeof along varilable name)
